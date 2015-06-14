a8ce61791a7973a9ebc59341b71747658fdefe8c
sami purmonen <purmonen@kth.se>
2015-06-14 02:57:52 +0200

    Changed some stuff in events

--- Teststs/ArmadaEventDetailTableViewController.swift
+++ Teststs/ArmadaEventDetailTableViewController.swift
@@ -49,7 +49,7 @@ class ArmadaEventDetailTableViewController: UITableViewController {
             let timeFormatter = NSDateFormatter()
             timeFormatter.dateFormat = "HH:mm"
             
-            cell.eventImageView.image = UIImage(named: selectedArmadaEvent.title)
+//            cell.eventImageView.image = UIImage(named: selectedArmadaEvent.title)
 //            cell.locationLabel.text = selectedArmadaEvent.location + ", " + timeFormatter.stringFromDate(selectedArmadaEvent.startDate) + " - " + timeFormatter.stringFromDate(selectedArmadaEvent.endDate)
             
             if selectedArmadaEvent.location.isEmpty {
