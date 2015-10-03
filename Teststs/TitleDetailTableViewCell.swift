33272451e40d1f7b4cc3c178617d4e7aea7d0ee4
sami purmonen <purmonen@kth.se>
2015-10-03 23:57:07 +0200

    Fixed stuff

---
+++ Teststs/TitleDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddCompanyPropertyTableViewCell.swift
+//  Armada
+//
+//  Created by Sami Purmonen on 03/10/15.
+//  Copyright © 2015 Sami Purmonen. All rights reserved.
+//
+
+import UIKit
+
+class AddCompanyPropertyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
