bf5337aa901422fcfd33464b556dcdbc09c17b6b
sami purmonen <purmonen@kth.se>
2015-10-08 21:11:54 +0200

    Added catalogue filter empty view

--- Teststs/MatchTableViewController.swift
+++ Teststs/MatchTableViewController.swift
@@ -2,38 +2,104 @@ import UIKit
 
 
 private let MatchFilter = _CompanyFilter(userDefaultsKey: "MatchFilter")
+private let FakeMatchFilter = _CompanyFilter(userDefaultsKey: "FakeMatchFilter")
+
 class MatchTableViewController: UITableViewController {
     
+    @IBOutlet weak var filterButton: UIBarButtonItem!
     var companiesWithMatchPercentages = [(company: Company, percentage: Double)]()
     var matchPercentages = [Double]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if FakeMatchFilter.isEmpty {
+            FakeMatchFilter[CompanyProperty.WorkFields] = Array(ArmadaApi.workFields[0...3])
+            FakeMatchFilter[CompanyProperty.CompanyValues] = Array(ArmadaApi.companyValues[0...2])
+            FakeMatchFilter[CompanyProperty.WorkWays] = Array(ArmadaApi.workWays[0...2])
+        }
     }
+    
+    var label: UILabel?
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         let companiesWithMatchPercentages = calculateCompaniesWithMatchPercentages().filter { $0.percentage > 0 }
         self.companiesWithMatchPercentages = Array(companiesWithMatchPercentages[0..<min(10, companiesWithMatchPercentages.count)])
         self.updateFavoritesUI()
+        
+        let labelTag = 1337
+        let overlayTag = 1338
+        
+        for subview in view.subviews {
+            if subview.tag == labelTag || subview.tag == overlayTag {
+                subview.removeFromSuperview()
+            }
+        }
+        
+        
+        filterButton.enabled = !MatchFilter.isEmpty
+        filterButton.title = MatchFilter.isEmpty ? nil : "Filter"
+        
+        if MatchFilter.isEmpty {
+            let overlay = UIView(frame: view.frame)
+            overlay.alpha = 0.4
+            overlay.tag = overlayTag
+            overlay.backgroundColor = UIColor.blackColor()
+            view.addSubview(overlay)
+            view.addConstraint(NSLayoutConstraint(item: overlay, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: overlay, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: overlay, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: overlay, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 1, constant: 0))
+            view.backgroundColor = UIColor.blackColor()
+            
+            let label = UIButton(frame: view.frame)
+            label.setTitle("Start Matching", forState: .Normal)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.backgroundColor = ColorScheme.armadaGreen
+            
+            label.titleLabel?.font = UIFont.systemFontOfSize(30)
+            
+            let layer = label.layer
+            layer.cornerRadius = 10
+            let topPadding: CGFloat = 20
+            let leadingPadding: CGFloat = 20
+            label.contentEdgeInsets = UIEdgeInsets(top: topPadding, left: leadingPadding, bottom: topPadding, right: leadingPadding)
+            
+            
+            
+            
+            label.tag = labelTag
+            label.addTarget(self, action: "openFilter:", forControlEvents: UIControlEvents.TouchUpInside)
+            view.addSubview(label)
+            view.bringSubviewToFront(label)
+            view.addConstraint(NSLayoutConstraint(item: label, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1, constant: -64))
+        }
+        
         self.tableView.reloadData()
     }
     
+    func openFilter(sender: AnyObject) {
+        performSegueWithIdentifier("FilterSegue", sender: nil)
+    }
+    
     func calculateCompaniesWithMatchPercentages() -> [(company: Company, percentage: Double)] {
-        if MatchFilter.isEmpty {
-            return []
-        }
+        
+        
+        
+        let Filter = MatchFilter.isEmpty ? FakeMatchFilter : MatchFilter
+        
         var matches = [(company: Company, percentage: Double)]()
         for company in ArmadaApi.companies {
             var percentage = 1.0
             for companyProperty in CompanyProperty.All {
-                for value in MatchFilter[companyProperty] {
+                for value in Filter[companyProperty] {
                     if !company[companyProperty].contains(value) {
                         percentage *= companyProperty.penalty
                     }
                 }
             }
-            for armadaField in MatchFilter.armadaFields {
+            for armadaField in Filter.armadaFields {
                 if !company.hasArmadaFieldType(armadaField) {
                     percentage *= 0.8
                 }
