6e1eae398290c8d281f9a654dbc5005e484031a8
rfklinc <rklinc@kth.se>
2017-09-15 20:54:44 +0200

    Added About information to About Tab

--- Teststs/AboutCollectionViewController.swift
+++ Teststs/AboutCollectionViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class AboutCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    @IBOutlet weak var aboutText: UITextView!
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.view.backgroundColor = UIColor.red
@@ -83,9 +84,22 @@ class AboutCollectionViewController: UICollectionViewController, UICollectionVie
                 let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
                                                                                  withReuseIdentifier: "AboutHeaderCollectionReusableView",
                                                                                  for: indexPath) as! AboutHeaderCollectionReusableView
-                headerView.noticeLabel.text = "hardcoded content goes here"
-                let designGrey = UIColor(red: 0xF8/255, green: 0xF7/255, blue: 0xF7/255, alpha: 0xFF)
-                headerView.backgroundColor = designGrey
+                let about = "<font size='4' line-height: '0.7'><b>Started in 1981,</b> THS Armada is a part of the student union THS and the largest student driven project at KTH The Royal Institute of Technology. The fair was first organised in 1981 and has been a recurring event at KTH ever since. The project has a turnover of more than 6 million SEK and involves more than 300 students.\n\n<b>Aside from the fair</b>, THS Armada has several other events, all to give you the best possible chance of finding your <i>dream</i> employer.</font>"
+                let diversity = "<font size = '4' line-height: '0.7'>Exhibitors with this icon have expressed a focus on <b>diversity</b> in their organization and/or business and operations.</font>" //Events with this icon will be focused on topics regarding diversity.
+                
+                let sustainability = "<font size = '4' line-height: '0.7'>Exhibitors with this icon have expressed a focus on <b>sustainability</b> in their organization and/or business and operations.</font>"
+               // Events with this icon will be focused on topics regarding sustainability."
+                
+                let quality = "<font size = '4' line-height: '0.7'>Exhibitors with this icon have expressed a focus on <b>quality</b> in their organization and/or business and operations.</font>"
+                //Events with this icon will be focused on topics regarding quality."
+                headerView.aboutText.attributedText =  about.attributedHtmlString
+                headerView.aboutText.isScrollEnabled = false
+                headerView.diversityText.attributedText = diversity.attributedHtmlString
+                headerView.diversityText.isScrollEnabled = false
+                headerView.sustainabilityText.attributedText = sustainability.attributedHtmlString
+                headerView.sustainabilityText.isScrollEnabled = false
+                headerView.qualityText.attributedText = quality.attributedHtmlString
+                headerView.qualityText.isScrollEnabled = false
                 return headerView
             default:
                 //4
