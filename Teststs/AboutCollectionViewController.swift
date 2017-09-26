34a9000ac0c7ae9eaff119ccebd6f13b5a5fe5b6
Ola Roos <olaroos@fastmail.fm>
2017-09-26 21:53:43 +0200

    changed about header'

--- Teststs/AboutCollectionViewController.swift
+++ Teststs/AboutCollectionViewController.swift
@@ -25,9 +25,8 @@ class AboutCollectionViewController: UICollectionViewController, UICollectionVie
         // setup datasource = SponsorCollectionViewDataSource
         dataSource = ArmadaSponsorCollectionViewDataSource(collectionViewController: self)
         collectionView?.dataSource = dataSource
-        
-        // setup header title
-        let frame = CGRect(x: 0,y: 0, width: 200, height: 100);
+        // fix header
+        let frame = CGRect(x: 0,y: 13, width: 200, height: 30);
         let label = UILabel(frame: frame)
         let myMutableString = NSMutableAttributedString(
             string: "A B O U T THS Armada 2017",
@@ -37,34 +36,30 @@ class AboutCollectionViewController: UICollectionViewController, UICollectionVie
         myMutableString.addAttribute(NSFontAttributeName, value: UIFont(name: "BebasNeueRegular", size: 22.0), range:NSRange(location: 0, length: 9))
         label.textAlignment = .center
         label.attributedText = myMutableString
-        self.navigationItem.titleView = label
+        let newTitleView = UIView(frame: CGRect(x: 0, y:0 , width: 200, height: 50))
+        newTitleView.addSubview(label)
+        self.navigationItem.titleView = newTitleView
         
         // setup header left logo
         var armadalogo:UIImage = #imageLiteral(resourceName: "armada_round_logo_green.png")
         let headerHeight:CGFloat = (self.navigationController?.navigationBar.frame.size.height)!
-        let headerImgSize = headerHeight * 0.8
+        let headerImgSize = headerHeight * 0.7
+        
+        // change size of armada logo
         let newSize = CGSize(width: headerImgSize, height: headerImgSize)
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
         armadalogo.draw(in: CGRect(x: 0, y: 0, width: headerImgSize, height: headerImgSize))
         let newarmadalogo = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        // setup header bottom border        
-        let bottomBorderH:CGFloat = 0.75
-        let bottomBorderRect = CGRect(x: 0, y: headerHeight, width: UIScreen.main.bounds.width, height: bottomBorderH)
-        let bottomBorderView = UIView(frame: bottomBorderRect)
-        bottomBorderView.backgroundColor = ColorScheme.navbarBorderGrey
-        self.navigationController?.navigationBar.addSubview(bottomBorderView)
-        
         // add armada logo to header
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image:newarmadalogo , style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: newarmadalogo, style: UIBarButtonItemStyle.done, target: nil, action: nil)
         
         // change status bar background color
-        let statusGrey = UIColor(hex: 0x999A99)
         let statusView = UIView(frame: CGRect(x:0, y:0, width: 500, height: 20))
         statusView.backgroundColor = .black
         self.navigationController?.view.addSubview(statusView)
-
+        
     }
 
     // DELEGATE methods:
