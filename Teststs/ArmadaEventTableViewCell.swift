e619ea3b3e4e0e10e400708e2ee0b2e9dbd9e5a5
rfklinc <rklinc@kth.se>
2017-09-29 21:55:44 +0200

    Added temporary event feed

---
+++ Teststs/ArmadaEventTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArmadaEventTableViewCell.swift
+//  Armada
+//
+//  Created by Rebecca Forstén Klinc on 2017-09-29.
+//  Copyright © 2017 Rebecca Forstén Klinc. All rights reserved.
+//
+
+import UIKit
+
+class ArmadaEventTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var eventImageView: UIImageView!
+}
+
