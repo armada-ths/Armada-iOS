0e0a86d8e2e2eec1da48f5eabe046e4f8a4f2f1b
Boran Karaca <boran@Borans-MacBook-Pro.local>
2018-11-05 15:36:24 +0100

    Added exhibitor detail view

--- Armada/Exhibitor/ExhibitorDetailViewController.swift
+++ Armada/Exhibitor/ExhibitorDetailViewController.swift
@@ -2,6 +2,14 @@ import SDWebImage
 import UIKit
 
 class ExhibitorDetailViewController: UIViewController {
+    
+    @IBOutlet weak var industriesTextView: UITextView!
+    @IBOutlet weak var employmentsTextView: UITextView!
+    @IBOutlet weak var contactPhoneLabel: UILabel!
+    @IBOutlet weak var contactEmailLabel: UILabel!
+    @IBOutlet weak var contactPersonLabel: UILabel!
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var exhibitorAboutTextView: UITextView!
     var exhibitor: Exhibitor? {
         didSet {
             updateData()
@@ -10,11 +18,51 @@ class ExhibitorDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         self.navigationController?.navigationBar.isHidden = false
+        
+        //set logo
+        let urlString = "https://ais.armada.nu" + (exhibitor?.logoSquared)!
+        guard let url = URL(string: urlString) else {return }
+        logo.sd_setImage(with: url)
+        
+        //set about
+        exhibitorAboutTextView.text = exhibitor?.about
+        
+        //set contact
+        contactPersonLabel.text = "Contact Person: \(exhibitor?.contactName ?? "")"
+        contactEmailLabel.text = "Email : \(exhibitor?.contactEmail ?? "")"
+        contactPhoneLabel.text = "Phone: \(exhibitor?.contactPhoneNumber ?? "")"
+        
+        
+        
+        //set employment types
+        var employments = ""
+        
+        for em in (exhibitor?.employments)! {
+            employments += em.name + ", "
+        }
+        employments = employments.trimmingCharacters(in: .whitespacesAndNewlines)
+        employments = employments.trimmingCharacters(in: [","])
+        employmentsTextView.text = employments
+        
+        //set industries
+        
+        var industries = ""
+        
+        for ind in (exhibitor?.industries)! {
+            industries += ind.name + ", "
+        }
+        industries = industries.trimmingCharacters(in: .whitespacesAndNewlines)
+        industries = industries.trimmingCharacters(in: [","])
+        industriesTextView.text = industries
+        
+        
+        
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print(exhibitor?.name)
+        
+        //print(exhibitor?.name)
     }
 
     func updateData() {
